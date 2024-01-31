d0190422cddbf01d1866baa0146891706d743d9e
Ravin01 <ravinganapathi@gmail.com>
2024-01-31 14:25:09 +0530

    updated

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -27,7 +27,7 @@ const Profile = () => {
         <div className="profile-buttons">
           <button className="profile-btn">
             <Link
-              to="https://drive.google.com/file/d/1hkbb7o-umvn7K7fqkTtVJqdDURvrMWLK/view?usp=sharing"
+              to="https://drive.google.com/file/d/1L-SADmZr2uXSWmjDM49t1vEyEvng-sva/view?usp=sharing"
               target="blank"
               className="profile-btn-link"
             >
